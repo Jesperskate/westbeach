cb21f9ad0b0cb6ad7646115fe80d2851a2142877
Jesperskate <jvoorendt@gmail.com>
2015-11-24 17:12:31 +0100

    Date picker

--- client/templates/rooster.js
+++ client/templates/rooster.js
@@ -2,8 +2,11 @@ Template.rooster.events({
   'submit': function(event, template) {
     event.preventDefault();
     var sport = template.$('[name=sport]').val();
-    var date = template.$('[name=datepicker]').val();
+    var date = template.$('[name=date]').val();
+    var starttime = template.$('[name=time]').val();
     var level = template.$('[name=level]').val();
+    var capaciteit = template.$('[name=capaciteit]').val();
+
 
 
 
@@ -11,6 +14,17 @@ Template.rooster.events({
 
     console.log(sport+" en "+date);
     
+         // Insert a task into the collection
+      Lessen.insert({
+        sport: sport,
+        date: date,
+        starttime:starttime,
+        level:level,
+        capaciteit:capaciteit,
+        createdAt: new Date() // current time
+      });
+ 
+
 
   }
 });
