2014b2144ab75a44445529e603538dce53cbe273
RKalicharan <rs.kalicharan@outlook.com>
2016-03-19 16:54:21 +0100

    Small changes, haallp!

--- client/templates/rooster.js
+++ client/templates/rooster.js
@@ -51,8 +51,8 @@ Template.rooster.events({
 });
 
 Template.rooster.rendered = function() {
-    $('.datepicker').datetimepicker({useCurrent: true, format: 'DD/MM/YYYY'});
-    $('.timepicker').datetimepicker({useCurrent: true, format: 'LT'});
+    $('.datepicker').datetimepicker({useCurrent: true, allowInputToggle: true, format: 'DD/MM/YYYY'});
+    $('.timepicker').datetimepicker({useCurrent: true, allowInputToggle: true, format: 'LT'});
 
 
 }
