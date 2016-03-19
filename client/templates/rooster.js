ac70dfb84fc13c3a716bbb9cef6241c94b07a835
RKalicharan <rs.kalicharan@outlook.com>
2016-03-19 16:47:22 +0100

    Revert "Maybe? Faulty DateTimePicker"
    This reverts commit 6558f897a219b0ee9fc08e94c6c9c26f2bdf72ba.

--- client/templates/rooster.js
+++ client/templates/rooster.js
@@ -51,7 +51,6 @@ Template.rooster.events({
 });
 
 Template.rooster.rendered = function() {
-<<<<<<< Updated upstream
     $('.datepicker').datetimepicker({useCurrent: true, format: 'DD/MM/YYYY'});
     $('.timepicker').datetimepicker({useCurrent: true, format: 'LT'});
 
@@ -74,8 +73,3 @@ addLes: function (text) {
   }
 
 });
-=======
-    $('.datepicker').datetimepicker({useCurrent: true, allowInputToggle: true, format: 'DD/MM/YYYY'});
-    $('.timepicker').datetimepicker({useCurrent: true, allowInputToggle: true, format: 'LT'});
-}
->>>>>>> Stashed changes
