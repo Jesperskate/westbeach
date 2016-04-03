6a95f2e42ed1cad3ad52e5b3cc56e4ac36aadca0
Jesper Voorendt <jesper@mindworkmedia.nl>
2016-04-03 20:41:00 +0200

    Pagina's toegevoegd en css edits

--- client/templates/lessen.js
+++ client/templates/lessen.js
@@ -15,6 +15,21 @@ Template.golfsurfen.helpers({
 		return Lessen.find({sport:'golfsurfen'});
 	}
 
+});	
+Template.bootcamp.helpers({
+	lessen: function(){
+		return Lessen.find({sport:'bootcamp'});
+	}
+
+});
+
+
+//code for golfsurfen
+Template.yoga.helpers({
+	lessen: function(){
+		return Lessen.find({sport:'yoga'});
+	}
+
 });
 
 Template.les.helpers({
@@ -24,7 +39,18 @@ Template.les.helpers({
 	aantalDeelnemers: function(){
 		return Aanmeldingen.find({ idLes: this._id}).count();
 
-	}
+	},
+	 displaydate: function (datemongo) {
+	    var date = datemongo.slice(0,5);
+	    var monthNames = [ "January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December" ];
+
+		var monthNr = datemongo.slice(4,5);
+
+		var month = monthNames[monthNr-1];
+
+	    return date +month;
+ 	 }
 
 })
 
