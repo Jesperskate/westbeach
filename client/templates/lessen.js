a0445d67019e1c73c33df74adee4ab30ca4dab78
RKalicharan <rs.kalicharan@outlook.com>
2016-04-16 17:29:30 +0200

    Gefikst?

--- client/templates/lessen.js
+++ client/templates/lessen.js
@@ -10,7 +10,6 @@ Template.kitesurfen.helpers({
 
 });
 
-
 //code for golfsurfen
 Template.golfsurfen.helpers({
 	lessen: function(){
@@ -128,21 +127,6 @@ Template.aanmelding.events({
     	}    
 	 }   
   });
-
-Meteor.subscribe('publicAanmeldingen');
-		Template.lessen.helpers({
-			aangemeld: function(){
-		// Om lessen op de juiste datum volgorde te zetten, moet eerst de input naar 'date' worden veranderd
-				// van dd/mm/jjjj naar een js date()... want nu pakt het alleen de dag en niet de maand.
-				
-				var userEmail = Meteor.user().emails[0].address;
-
-				 console.log(userEmail);
-				
-				return Aanmeldingen.find({email: userEmail});
-			}
-
-		});
 
 Meteor.methods({
 
