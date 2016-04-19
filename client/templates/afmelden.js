b22e84d23916d7580add95eaebd72f8ff4793ca9
RKalicharan <rs.kalicharan@outlook.com>
2016-04-19 12:41:54 +0200

    Admin kan iedereen afmelden, normale gebruikers zien alleen zichzelf.

--- client/templates/afmelden.js
+++ client/templates/afmelden.js
@@ -12,7 +12,17 @@ if(Meteor.isClient){
 
 				 console.log(userEmail);
 				
-				return Aanmeldingen.find({email: userEmail});
+				return Aanmeldingen.find({mail: userEmail});
+			},
+			Adminaangemeld: function(){
+		// Om lessen op de juiste datum volgorde te zetten, moet eerst de input naar 'date' worden veranderd
+				// van dd/mm/jjjj naar een js date()... want nu pakt het alleen de dag en niet de maand.
+				
+				var userEmail = Meteor.user().emails[0].address;
+
+				 console.log(userEmail);
+				
+				return Aanmeldingen.find({});
 			}
 
 		});
